f43cfafeae079d2013ae51c07941650c16796753
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-28 22:28:47 +0430

    Create an almost blank component named Desk

---
+++ src/pages/Desk/Desk.jsx
@@ -0,0 +1,11 @@
+import styles from './Desk.module.css'
+
+const Desk = () => {
+  return (
+    <>
+      Desk
+    </>
+  )
+}
+
+export default Desk
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Desk from './Desk/Desk'
+
+export {
+  Desk,
+}
