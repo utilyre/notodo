59268313555dba601c3c3da6c197c8bade8074c4
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-29 00:48:59 +0430

    Add modify functions to useTodos hook
    Added functions:
    - add
    - del
    - edit

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -3,12 +3,45 @@ import { useState, useEffect } from 'react'
 const useTodos = (key) => {
   const [todos, setTodos] = useState([])
 
+  const add = (note) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 0
+
+    setTodos([
+      ...todos,
+      {
+        id: lastId + 1,
+        note: note,
+        done: false,
+        createdAt: Date.now(),
+      },
+    ])
+  }
+
+  const del = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const edit = (id, changes) => {
+    const todo = todos.find((todo) => todo.id === id)
+    const index = todos.indexOf(todo)
+
+    setTodos([
+      ...todos.slice(0, index),
+      { ...todo, ...changes },
+      ...todos.slice(index + 1),
+    ])
+  }
+
   useEffect(() => {
     const cache = localStorage.getItem(key)
     if (cache !== null) setTodos(JSON.parse(cache))
   }, [])
 
-  return [todos]
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(todos))
+  }, [todos])
+
+  return [todos, add, del, edit]
 }
 
 export default useTodos
