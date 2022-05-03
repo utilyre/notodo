f842fe83d81f328a03637feda689192fab1efe4f
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-04 00:11:57 +0430

    Make Desk page including a form and a todolist

--- src/pages/Desk/Desk.jsx
+++ src/pages/Desk/Desk.jsx
@@ -1,9 +1,57 @@
+import { useRef, useState, useEffect } from 'react'
+
 import styles from './Desk.module.css'
+import { Todo } from '../../components'
+import { useTodos } from '../../hooks'
 
 const Desk = () => {
+  const domTodoTobe = useRef()
+  const [todos, addTodo, delTodo, editTodo] = useTodos('todos')
+  const [todoTobe, setTodoTobe] = useState('')
+
+  useEffect(() => {
+    domTodoTobe.current.focus()
+  }, [])
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    addTodo(todoTobe)
+    setTodoTobe('')
+  }
+
   return (
-    <main>
-      Desk
+    <main className={styles.container}>
+      <form
+        onSubmit={onSubmit}
+        className={styles.form}
+      >
+        <input
+          type='text'
+          required
+          placeholder='Watcha wanna do?'
+          ref={domTodoTobe}
+          value={todoTobe}
+          onChange={(e) => setTodoTobe(e.target.value)}
+          className={styles.todoTobe}
+        />
+
+        {/* <button */}
+        {/*   type='submit' */}
+        {/*   className={styles.add} */}
+        {/* > */}
+        {/*   Add */}
+        {/* </button> */}
+      </form>
+
+      {todos.map((todo) =>
+        <Todo
+          key={todo.id}
+          todo={todo}
+          delTodo={delTodo}
+          editTodo={editTodo}
+        />
+      )}
     </main>
   )
 }
